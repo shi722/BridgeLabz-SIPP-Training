0653f5854cd69abf56965b43c2f3c12440d1e050
shi722 <shivanshusolanki910@gmail.com>
2025-06-17 09:20:02 +0530

    [Shivanshu] Que17

---
+++ Day2_ControlFlow/CountdownWhile.java
@@ -0,0 +1,13 @@
+package Day2_ControlFlow;
+import java.util.Scanner;
+public class CountdownWhile {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int counter = sc.nextInt();
+        while (counter >= 1) {
+            System.out.println(counter);
+            counter--;
+        }
+        System.out.println("Lift off!");
+    }
+}
