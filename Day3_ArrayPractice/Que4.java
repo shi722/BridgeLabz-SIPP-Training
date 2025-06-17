040238f0f469fdbbb9e6a00d557ee1d61e1429a0
shi722 <shivanshusolanki910@gmail.com>
2025-06-17 17:22:39 +0530

    [Shivanshu] Que4 MultiplicationTable

---
+++ Day3_ArrayPractice/Que4.java
@@ -0,0 +1,20 @@
+package Day3_ArrayPractice;
+
+import java.util.Scanner;
+public class Que4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+
+        int[] table = new int[10];
+        for (int i = 1; i <= 10; i++) {
+            table[i - 1] = number * i;
+        }
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(number + " * " + i + " = " + table[i - 1]);
+        }
+    }
+}
+
