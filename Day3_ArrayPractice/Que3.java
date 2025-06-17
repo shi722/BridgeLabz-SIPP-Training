7c79bd247d4b153a9ed843853a778b327d622194
shi722 <shivanshusolanki910@gmail.com>
2025-06-17 17:11:28 +0530

    [Shivanshu] Que3 FizzBuzz

---
+++ Day3_ArrayPractice/Que3.java
@@ -0,0 +1,34 @@
+package Day3_ArrayPractice;
+
+import java.util.Scanner;
+
+public class Que3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+
+        if (number <= 0) {
+            System.out.println("Please enter a positive number.");
+            return;
+        }
+
+        String[] results = new String[number + 1];
+
+        for (int i = 0; i <= number; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                results[i] = "FizzBuzz";
+            } else if (i % 3 == 0) {
+                results[i] = "Fizz";
+            } else if (i % 5 == 0) {
+                results[i] = "Buzz";
+            } else {
+                results[i] = String.valueOf(i);
+            }
+        }
+
+        for (int i = 1; i <= number; i++) {
+            System.out.println("Position " + i + " = " + results[i]);
+        }
+    }
+}
