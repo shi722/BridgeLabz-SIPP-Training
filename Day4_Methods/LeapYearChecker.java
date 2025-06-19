24fc3742b91dae3238148eaceb84fc6cc3702e4d
Shivanshu Solanki <shivanshusolanki910@gmail.com>
2025-06-19 10:03:41 +0530

    Shivanshu Que6 LeapYear

---
+++ Day4_Methods/LeapYearChecker.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class LeapYearChecker {
+
+    public static boolean isLeapYear(int year) {
+        if (year < 1582) {
+            return false; // Gregorian calendar starts from 1582
+        }
+        return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a year (>= 1582): ");
+        int year = sc.nextInt();
+
+        if (year < 1582) {
+            System.out.println("Year must be 1582 or later. Not a Leap Year.");
+        } else if (isLeapYear(year)) {
+            System.out.println(year + " is a Leap Year.");
+        } else {
+            System.out.println(year + " is Not a Leap Year.");
+        }
+    }
+}
