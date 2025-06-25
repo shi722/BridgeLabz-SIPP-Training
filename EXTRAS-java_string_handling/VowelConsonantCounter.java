37043d019127180759bf7dd98d7128e9dc5e618b
Shivanshu Solanki <shivanshusolanki910@gmail.com>
2025-06-25 10:00:53 +0530

    Shivanshu Que1

---
+++ EXTRAS-java_string_handling/VowelConsonantCounter.java
@@ -0,0 +1,14 @@
+public class VowelConsonantCounter {
+    public static void countVC(String str) {
+        int vowels = 0, consonants = 0;
+        str = str.toLowerCase();
+        for (char c : str.toCharArray()) {
+            if (Character.isLetter(c)) {
+                if ("aeiou".indexOf(c) != -1) vowels++;
+                else consonants++;
+            }
+        }
+        System.out.println("Vowels: " + vowels);
+        System.out.println("Consonants: " + consonants);
+    }
+}
