749a279188cf1ee46ca0a420d6bc6b7601618b19
Shivanshu Solanki <shivanshusolanki910@gmail.com>
2025-06-25 10:02:03 +0530

    Shivanshu Que2

---
+++ EXTRAS-java_string_handling/ReverseString.java
@@ -0,0 +1,9 @@
+public class ReverseString {
+    public static String reverse(String str) {
+        String rev = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            rev += str.charAt(i);
+        }
+        return rev;
+    }
+}
