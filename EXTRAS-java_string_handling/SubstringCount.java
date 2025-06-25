d029fa18731c6fca2e490c2e3d5cb77532208467
Shivanshu Solanki <shivanshusolanki910@gmail.com>
2025-06-25 10:06:33 +0530

    Shivanshu Que6

---
+++ EXTRAS-java_string_handling/SubstringCount.java
@@ -0,0 +1,10 @@
+public class SubstringCount {
+    public static int countOccurrences(String str, String sub) {
+        int count = 0, index = 0;
+        while ((index = str.indexOf(sub, index)) != -1) {
+            count++;
+            index += sub.length();
+        }
+        return count;
+    }
+}
