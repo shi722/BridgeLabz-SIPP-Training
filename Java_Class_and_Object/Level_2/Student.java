372ccc37951cf579047110d11b3c367778d859b5
Shivanshu Solanki <shivanshusolanki910@gmail.com>
2025-06-26 12:40:40 +0530

    Shivanshu Java Class and Object Created

---
+++ Java_Class_and_Object/Level_2/Student.java
@@ -0,0 +1,38 @@
+package Java_Class_and_Object.Level_2;
+
+public class Student {
+    String name;
+    int rollNumber;
+    double marks;
+
+    Student(String name, int rollNumber, double marks) {
+        this.name = name;
+        this.rollNumber = rollNumber;
+        this.marks = marks;
+    }
+
+    char calculateGrade() {
+        if (marks >= 90) {
+            return 'A';
+        } else if (marks >= 75) {
+            return 'B';
+        } else if (marks >= 60) {
+            return 'C';
+        } else if (marks >= 40) {
+            return 'D';
+        } else {
+            return 'F';
+        }
+    }
+    void displayDetails() {
+        System.out.println("Student Name: " + name);
+        System.out.println("Roll Number: " + rollNumber);
+        System.out.println("Marks: " + marks);
+        System.out.println("Grade: " + calculateGrade());
+    }
+
+    public static void main(String[] args) {
+        Student s1 = new Student("Shivanshu", 101, 62.5);
+        s1.displayDetails();
+    }
+}
