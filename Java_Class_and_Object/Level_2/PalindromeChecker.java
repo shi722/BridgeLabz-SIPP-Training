dae31a45242c01a460beaea1c70ef8c46ea1e2fc
Shivanshu Solanki <shivanshusolanki910@gmail.com>
2025-06-26 12:55:10 +0530

    Shivanshu Que3

---
+++ Java_Class_and_Object/Level_2/PalindromeChecker.java
@@ -0,0 +1,27 @@
+package Java_Class_and_Object.Level_2;
+
+public  class PalindromeChecker {
+    String text;
+
+    PalindromeChecker(String text) {
+        this.text = text;
+    }
+
+    boolean isPalindrome() {
+        String reversed = new StringBuilder(text).reverse().toString();
+        return text.equalsIgnoreCase(reversed);
+    }
+
+    void displayResult() {
+        if (isPalindrome()) {
+            System.out.println("\"" + text + "\" is a palindrome.");
+        } else {
+            System.out.println("\"" + text + "\" is not a palindrome.");
+        }
+    }
+
+    public static void main(String[] args) {
+        PalindromeChecker checker = new PalindromeChecker("Madam");
+        checker.displayResult();
+    }
+}
