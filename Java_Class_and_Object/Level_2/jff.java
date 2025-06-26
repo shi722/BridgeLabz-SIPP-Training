74865a9cd63605a67f670d3bf06d0d131fe30e37
Shivanshu Solanki <shivanshusolanki910@gmail.com>
2025-06-26 12:52:03 +0530

    Shivanshu Que2

---
+++ Java_Class_and_Object/Level_2/jff.java
@@ -0,0 +1,33 @@
+package Java_Class_and_Object.Level_2;
+
+class Student {
+    String name;
+    int rollNumber;
+    double marks;
+
+    Student(String name, int rollNumber, double marks) {
+        this.name = name;
+        this.rollNumber = rollNumber;
+        this.marks = marks;
+    }
+
+    char calculateGrade() {
+        if (marks >= 90) return 'A';
+        else if (marks >= 75) return 'B';
+        else if (marks >= 60) return 'C';
+        else if (marks >= 40) return 'D';
+        else return 'F';
+    }
+
+    void displayDetails() {
+        System.out.println("Name: " + name);
+        System.out.println("Roll Number: " + rollNumber);
+        System.out.println("Marks: " + marks);
+        System.out.println("Grade: " + calculateGrade());
+    }
+
+    public static void main(String[] args) {
+        Student s = new Student("Shivanshu", 101, 63);
+        s.displayDetails();  
+    }
+}
