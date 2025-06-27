c55c7f9411696a277448d1427da45034ba97dac3
Shivanshu Solanki <shivanshusolanki910@gmail.com>
2025-06-27 08:56:34 +0530

    Shivanshu Que2

---
+++ this_static_final_key_words_and_instanceof_Operator/Book.java
@@ -0,0 +1,23 @@
+package this_static_final_key_words_and_instanceof_Operator;
+public class Book {
+    static String libraryName = "Central Library";
+    final String isbn;
+    String title, author;
+    Book(String title, String author, String isbn) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+    }
+
+    static void displayLibraryName() {
+        System.out.println("Library: " + libraryName);
+    }
+
+    void showBook() {
+        if (this instanceof Book) {
+            System.out.println("Title: " + title);
+            System.out.println("Author: " + author);
+            System.out.println("ISBN: " + isbn);
+        }
+    }
+}
