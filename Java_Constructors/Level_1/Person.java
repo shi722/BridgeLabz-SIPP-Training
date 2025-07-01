0bc2b61de1fda061492b2953e2983cf65d12d627
Shivanshu Solanki <shivanshusolanki910@gmail.com>
2025-07-01 12:50:25 +0530

    Shivanshu Que3

---
+++ Java_Constructors/Level_1/Person.java
@@ -0,0 +1,27 @@
+package Java_Constructors.Level_1;
+
+public class Person {
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Person(Person other) {
+        this.name = other.name;
+        this.age = other.age;
+    }
+
+    public void display() {
+        System.out.println("Name: " + name + ", Age: " + age);
+    }
+
+    public static void main(String[] args) {
+        Person p1 = new Person("Shivanshu", 21);
+        Person p2 = new Person(p1);
+        p1.display();
+        p2.display();
+    }
+}
