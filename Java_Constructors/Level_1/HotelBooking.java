a95959324d3b29a0e5e71ef5da6f822ae266e210
Shivanshu Solanki <shivanshusolanki910@gmail.com>
2025-07-01 12:51:05 +0530

    Shivanshu Que4

---
+++ Java_Constructors/Level_1/HotelBooking.java
@@ -0,0 +1,36 @@
+package Java_Constructors.Level_1;
+
+public class HotelBooking {
+    private String guestName;
+    private String roomType;
+    private int nights;
+
+    public HotelBooking() {
+        this("Guest", "Standard", 1);
+    }
+
+    public HotelBooking(String guestName, String roomType, int nights) {
+        this.guestName = guestName;
+        this.roomType = roomType;
+        this.nights = nights;
+    }
+
+    public HotelBooking(HotelBooking other) {
+        this.guestName = other.guestName;
+        this.roomType = other.roomType;
+        this.nights = other.nights;
+    }
+
+    public void display() {
+        System.out.println("Guest: " + guestName + ", Room Type: " + roomType + ", Nights: " + nights);
+    }
+
+    public static void main(String[] args) {
+        HotelBooking b1 = new HotelBooking();
+        HotelBooking b2 = new HotelBooking("Amit", "Deluxe", 5);
+        HotelBooking b3 = new HotelBooking(b2);
+        b1.display();
+        b2.display();
+        b3.display();
+    }
+}
