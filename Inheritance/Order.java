4579cba9d6ef120cc97fefbf9c6154ffd16a6690
Shivanshu Solanki <shivanshusolanki910@gmail.com>
2025-07-02 11:48:31 +0530

    Shivanshu Que3

---
+++ Inheritance/Order.java
@@ -0,0 +1,31 @@
+package Inheritance;
+public class Order {
+    String orderId;
+    String orderDate;
+
+    public Order(String orderId, String orderDate) {
+        this.orderId = orderId;
+        this.orderDate = orderDate;
+    }
+}
+class ShippedOrder extends Order {
+    String trackingNumber;
+
+    public ShippedOrder(String orderId, String orderDate, String trackingNumber) {
+        super(orderId, orderDate);
+        this.trackingNumber = trackingNumber;
+    }
+}
+
+class DeliveredOrder extends ShippedOrder {
+    String deliveryDate;
+
+    public DeliveredOrder(String orderId, String orderDate, String trackingNumber, String deliveryDate) {
+        super(orderId, orderDate, trackingNumber);
+        this.deliveryDate = deliveryDate;
+    }
+
+    public void getOrderStatus() {
+        System.out.println("Order ID: " + orderId + ", Delivered on: " + deliveryDate);
+    }
+}
