b9e01bbb76dae473436192fa2e152c891cb3a6d7
Shivanshu Solanki <shivanshusolanki910@gmail.com>
2025-07-02 11:50:15 +0530

    Shivanshu Que4

---
+++ Inheritance/Course.java
@@ -0,0 +1,34 @@
+package Inheritance;
+
+public class Course {
+    String courseName;
+    int duration;
+
+    public Course(String courseName, int duration) {
+        this.courseName = courseName;
+        this.duration = duration;
+    }
+}
+
+class OnlineCourse extends Course {
+    String platform;
+    boolean isRecorded;
+
+    public OnlineCourse(String courseName, int duration, String platform, boolean isRecorded) {
+        super(courseName, duration);
+        this.platform = platform;
+        this.isRecorded = isRecorded;
+    }
+}
+
+class PaidOnlineCourse extends OnlineCourse {
+    double fee;
+    double discount;
+
+    public PaidOnlineCourse(String courseName, int duration, String platform, boolean isRecorded, double fee, double discount) {
+        super(courseName, duration, platform, isRecorded);
+        this.fee = fee;
+        this.discount = discount;
+    }
+}
+
