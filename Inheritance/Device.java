19300d22e6f9ffab3ebb9dd810fb65e655344b3a
Shivanshu Solanki <shivanshusolanki910@gmail.com>
2025-07-02 11:46:55 +0530

    Shivanshu Que2

---
+++ Inheritance/Device.java
@@ -0,0 +1,29 @@
+package Inheritance;
+public class Device {
+    String deviceId;
+    String status;
+
+    public Device(String deviceId, String status) {
+        this.deviceId = deviceId;
+        this.status = status;
+    }
+
+    public void displayStatus() {
+        System.out.println("Device ID: " + deviceId + ", Status: " + status);
+    }
+}
+
+ class Thermostat extends Device {
+    int temperatureSetting;
+
+    public Thermostat(String deviceId, String status, int temperatureSetting) {
+        super(deviceId, status);
+        this.temperatureSetting = temperatureSetting;
+    }
+
+    @Override
+    public void displayStatus() {
+        super.displayStatus();
+        System.out.println("Temperature: " + temperatureSetting + "°C");
+    }
+}
