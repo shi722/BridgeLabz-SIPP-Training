24757aeeb642abc8c03fedd48b275c98a0c78285
Shivanshu Solanki <shivanshusolanki910@gmail.com>
2025-07-21 10:43:48 +0530

    Shivanshu Que 7

--- Day1_LinkedList/User.java
+++ Day1_LinkedList/User.java
@@ -1,5 +1,7 @@
 package Day1_LinkedList;
 
+import java.util.ArrayList;
+
 public class User {
     int userId;
     String name;
@@ -15,3 +17,4 @@ public class User {
         this.next = null;
     }
 }
+
