f063b3ae2bb1ae11ac687ae53bb338218b226c05
Shivanshu Solanki <shivanshusolanki910@gmail.com>
2025-07-21 10:47:56 +0530

    Shivanshu Que 9

---
+++ Day1_LinkedList/Ticket.java
@@ -0,0 +1,18 @@
+package Day1_LinkedList;
+
+public class Ticket {
+    int ticketId;
+    String customerName, movieName;
+    int seatNumber;
+    String bookingTime;
+    Ticket next;
+
+    Ticket(int ticketId, String customerName, String movieName, int seatNumber, String bookingTime) {
+        this.ticketId = ticketId;
+        this.customerName = customerName;
+        this.movieName = movieName;
+        this.seatNumber = seatNumber;
+        this.bookingTime = bookingTime;
+        this.next = null;
+    }
+}
