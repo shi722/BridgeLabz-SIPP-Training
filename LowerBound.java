4cb383a059e63068e6294fd1a00a468d823e7af8
Shivanshu Solanki <shivanshusolanki910@gmail.com>
2025-07-22 10:12:39 +0530

    Shivanshu Que 4

---
+++ LowerBound.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class LowerBound {
+    public static void addIntegers(List<? super Integer> list) {
+        list.add(10);
+        list.add(20);
+    }
+
+    public static void main(String[] args) {
+        List<Number> numberList = new ArrayList<>();
+        addIntegers(numberList);
+        System.out.println(numberList);
+    }
+}
