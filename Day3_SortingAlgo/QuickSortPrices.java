4655d239d4e3674446687df5357c9c1065190fce
Shivanshu Solanki <shivanshusolanki910@gmail.com>
2025-07-22 09:51:22 +0530

    Shivanshu Sorting

---
+++ Day3_SortingAlgo/QuickSortPrices.java
@@ -0,0 +1,32 @@
+package Day3_SortingAlgo;
+
+public class QuickSortPrices {
+    static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pi = partition(arr, low, high);
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
+    }
+
+    static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high], i = low - 1;
+
+        for (int j = low; j < high; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+                int t = arr[i]; arr[i] = arr[j]; arr[j] = t;
+            }
+        }
+
+        int t = arr[i + 1]; arr[i + 1] = arr[high]; arr[high] = t;
+        return i + 1;
+    }
+
+    public static void main(String[] args) {
+        int[] prices = {499, 199, 349, 899, 150};
+        quickSort(prices, 0, prices.length - 1);
+        for (int price : prices)
+            System.out.print(price + " ");
+    }
+}
