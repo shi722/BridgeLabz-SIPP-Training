aca14a922da8e7776aa2047bee78347e1e517e07
Shivanshu Solanki <shivanshusolanki910@gmail.com>
2025-07-22 10:05:44 +0530

    Que 1

---
+++ TypeSafeList.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public  class TypeSafeList {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("Hello");
+        String str = list.get(0);
+        System.out.println(str);
+    }
+}
