57cc1a70045fbe060c06e4f1fc00452137956efc
Shivanshu Solanki <shivanshusolanki910@gmail.com>
2025-07-22 10:00:18 +0530

    Shivanshu Que 9

---
+++ Day4_Linear_and_Binary_Search/RotationPoint.java
@@ -0,0 +1,13 @@
+package Day4_Linear_and_Binary_Search;
+
+public class RotationPoint {
+    public static int findMinIndex(int[] arr) {
+        int left = 0, right = arr.length - 1;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (arr[mid] > arr[right]) left = mid + 1;
+            else right = mid;
+        }
+        return left;
+    }
+}
