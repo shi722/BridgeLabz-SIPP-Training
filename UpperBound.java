4b742b24f2ff1d3413db59144133b650c48b28ee
Shivanshu Solanki <shivanshusolanki910@gmail.com>
2025-07-22 10:12:05 +0530

    Shivanshu  Que 3

---
+++ UpperBound.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class UpperBound {
+    public static void printNumbers(List<? extends Number> list) {
+        for (Number num : list)
+            System.out.println(num);
+    }
+
+    public static void main(String[] args) {
+        List<Integer> intList = Arrays.asList(1, 2, 3);
+        List<Double> doubleList = Arrays.asList(1.1, 2.2, 3.3);
+        printNumbers(intList);
+        printNumbers(doubleList);
+    }
+}
