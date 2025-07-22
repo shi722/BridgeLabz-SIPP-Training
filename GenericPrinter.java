1f1f5ef8498ac7d297eb1beeb0c794f78c391a61
Shivanshu Solanki <shivanshusolanki910@gmail.com>
2025-07-22 10:11:29 +0530

    Shivanshu

---
+++ GenericPrinter.java
@@ -0,0 +1,12 @@
+public class GenericPrinter {
+    public static <T> void print(T value) {
+        System.out.println(value);
+    }
+
+    public static void main(String[] args) {
+        print(42);
+        print("Java");
+        print(3.14);
+    }
+}
+
