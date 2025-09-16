c98ee3529f4b38e2fbf73a046cfc91ca8f8637ec
Shivanshu Solanki <shivanshusolanki910@gmail.com>
2025-09-16 21:48:01 +0530

    4Que

---
+++ PatientIDs.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+public class PatientIDs {
+    public static void main(String[] args) {
+        List<Integer> patientIds = Arrays.asList(101, 102, 103, 104);
+        patientIds.forEach(System.out::println);
+    }
+}
